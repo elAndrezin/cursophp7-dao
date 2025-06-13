88b3689830f609382d1dbbcca65417295de74118
andre <andrevirgilio11@gmail.com>
2025-06-13 14:05:24 -0500

    metodos de lista

--- classe/usuario.php
+++ classe/usuario.php
@@ -7,10 +7,12 @@ class Usuario
     private $dessenha;
     private $dtcadastro;
 
+    // Getters y Setters
     public function getIdusuario()
     {
         return $this->idusuario;
     }
+
     public function setIdusuario($value)
     {
         $this->idusuario = $value;
@@ -20,49 +22,92 @@ class Usuario
     {
         return $this->deslogin;
     }
+
     public function setDeslogin($value)
     {
         $this->deslogin = $value;
     }
+
     public function getDessenha()
     {
         return $this->dessenha;
     }
+
     public function setDessenha($value)
     {
         $this->dessenha = $value;
     }
+
     public function getDtcadastro()
     {
         return $this->dtcadastro;
     }
+
     public function setDtcadastro($value)
     {
         $this->dtcadastro = $value;
     }
 
+    // Métodos de negocio
     public function loadById($id)
     {
         $sql = new Sql();
-        $results = $sql->select("SELECT *FROM tb_usuarios where idusuario = :ID", array(":ID" => $id));
+        $results = $sql->select("SELECT * FROM tb_usuarios WHERE idusuario = :ID", array(":ID" => $id));
 
         if (count($results) > 0) {
-
             $row = $results[0];
-            $this->setIdusuario($row['idusuario']);
-            $this->setDeslogin($row['deslogin']);
-            $this->setDessenha($row['dessenha']);
-            $this->setDtcadastro(new DateTime($row['dtcadastro']));
+            $this->setData($row);
+        } else {
+            throw new Exception("Usuário não encontrado com ID: " . $id);
         }
+    }
+
+    public static function getList()
+    {
+        $sql = new Sql();
+        return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin");
+    }
+
+    public static function search($login)
+    {
+        $sql = new Sql();
+        return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array(
+            ':SEARCH' => "%" . $login . "%"
+        ));
+    }
+
+    public function login($login, $password)
+    {
+        $sql = new Sql();
+        $results = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :PASSWORD", array(
+            ":LOGIN" => $login,
+            ":PASSWORD" => $password
+        ));
+
+        if (count($results) > 0) {
+            $this->setData($results[0]);
+            return $this;
+        } else {
+            throw new Exception("Login ou senha inválidos");
+        }
+    }
+
+    // Métodos auxiliares
+    private function setData($data)
+    {
+        $this->setIdusuario($data['idusuario']);
+        $this->setDeslogin($data['deslogin']);
+        $this->setDessenha($data['dessenha']);
+        $this->setDtcadastro(isset($data['dtcadastro']) ? new DateTime($data['dtcadastro']) : null);
     }
 
     public function __toString()
     {
         return json_encode(array(
-            "idusuario"  => $this->getIdusuario(),
+            "idusuario" => $this->getIdusuario(),
             "deslogin" => $this->getDeslogin(),
             "dessenha" => $this->getDessenha(),
-            "dtcadastro" => $this->getDtcadastro()->format("d/m/Y H:i:s")
+            "dtcadastro" => $this->getDtcadastro() ? $this->getDtcadastro()->format("d/m/Y H:i:s") : null
         ));
     }
 }
--- index.php
+++ index.php
@@ -9,7 +9,22 @@ $usuarios = $sql->select("SELECT *FROM tb_usuarios");
 echo json_encode($usuarios);
 */
 require_once("config.php");
-$root = new Usuario();
-$root -> loadById(4);
 
-echo $root;
+// //Carrega um usuario
+// $root = new Usuario();
+// $root -> loadById(4);
+// echo $root;
+
+// Carrega uma lista de usuarios
+// $lista= Usuario::getList();
+// echo json_encode($lista);
+
+// carrega uma lista de usuarios buscando pelo login
+// $search = Usuario::search("ca");
+// echo json_encode($search);
+
+// carrega um usuario usando login e senha 
+$usuario = new Usuario();
+$usuario->login("carlos", "83294");
+
+echo $usuario;
